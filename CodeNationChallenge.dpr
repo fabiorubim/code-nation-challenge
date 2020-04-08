ad55fe9b6801fa61d9935b7ae034e94855392992
esoft13 - Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2020-04-08 01:01:10 -0300

    Send file error

--- CodeNationChallenge.dpr
+++ CodeNationChallenge.dpr
@@ -1,11 +1,11 @@
 program CodeNationChallenge;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
-  System.SysUtils, REST.Client, REST.Types, System.JSON, System.Hash;
+  System.SysUtils, REST.Client, REST.Types, System.JSON, System.Hash,
+  System.Classes;
 
 type
   TJSONRequestRec = record
@@ -18,7 +18,7 @@ var
   RESTRequest: TRESTRequest;
   RESTResponse: TRESTResponse;
   Input: String;
-  JSONValue : TJSonValue;
+  JSONValue: TJSonValue;
   JSONRequestRec: TJSONRequestRec;
 
 const
@@ -29,6 +29,7 @@ const
   C_POST_RESOURCE = 'v1/challenge/dev-ps/submit-solution';
   C_MY_TOKEN = 'e896178d5e18062430bb5522f916c28d3ab5418e';
   C_LINE = '==================================================';
+  C_FILE_NAME = 'answer.json';
 
 procedure Line;
 begin
@@ -37,18 +38,31 @@ begin
   Writeln;
 end;
 
+procedure SaveJSONtoFile(AJSON: string);
+var
+  StringList: TStringList;
+begin
+  StringList := TStringList.Create;
+  try
+    StringList.Add(AJSON);
+    StringList.SaveToFile(C_FILE_NAME);
+  finally
+    StringList.Free;
+  end;
+end;
+
 function GetJSON: string;
 begin
-  RESTClient:= TRESTClient.Create(C_GET_URL);
-  RESTRequest:= TRESTRequest.Create(nil);
-  RESTResponse:= TRESTResponse.Create(nil);
+  RESTClient := TRESTClient.Create(C_GET_URL);
+  RESTRequest := TRESTRequest.Create(nil);
+  RESTResponse := TRESTResponse.Create(nil);
   try
     try
-      RESTRequest.Client:= RESTClient;
-      RESTRequest.Response:= RESTResponse;
-      RESTRequest.Resource:= C_GET_RESOURCE;
+      RESTRequest.Client := RESTClient;
+      RESTRequest.Response := RESTResponse;
+      RESTRequest.Resource := C_GET_RESOURCE;
       RESTRequest.AddParameter('token', C_MY_TOKEN);
-      RESTRequest.Method:= rmGET;
+      RESTRequest.Method := rmGET;
       RESTRequest.Execute;
 
       if RESTResponse.StatusCode = 200 then
@@ -59,20 +73,16 @@ begin
         Writeln(JSONValue.ToJSON);
         Line;
 
-        JSONRequestRec.numero_casas        := JsonValue.GetValue<Integer>('numero_casas');
-        JSONRequestRec.token               := JsonValue.GetValue<string>('token');
-        JSONRequestRec.cifrado             := JsonValue.GetValue<string>('cifrado');
-        JSONRequestRec.decifrado           := JsonValue.GetValue<string>('decifrado');
-        JSONRequestRec.resumo_criptografico:= JsonValue.GetValue<string>('resumo_criptografico');
+        SaveJSONtoFile(JSONValue.ToJSON);
       end;
     finally
-      RESTResponse.Free;
-      RESTRequest.Free;
-      RESTResponse.Free;
+      FreeAndNil(RESTResponse);
+      FreeAndNil(RESTRequest);
+      FreeAndNil(RESTClient);
     end;
   except
     on E: Exception do
-     Writeln('Ocorreu um erro ao receber o JSON!');
+      Writeln('Ocorreu um erro ao receber o JSON!');
   end;
 end;
 
@@ -82,84 +92,123 @@ var
   I: Integer;
   NumChar: Byte;
 begin
-  QtChars:= ACipher.Length;
+  QtChars := ACipher.Length;
 
   for I := 1 to QtChars do
   begin
-    NumChar:= Ord(ACipher[I]);
-    if NumChar in [32, 46, 0..9] then
-      Result:= Result + Chr(NumChar)
+    NumChar := Ord(ACipher[I]);
+    if NumChar in [32, 46, 0 .. 9] then
+      Result := Result + Chr(NumChar)
     else
-      Result:= Result + Chr(NumChar - ANumPlace);
+      Result := Result + Chr(NumChar - ANumPlace);
   end;
 
-  Result:= Result.ToLower;
+  Result := Result.ToLower;
 end;
 
-function SetFieldValues:Boolean;
+procedure LoadJsonFile;
+var
+  StringList: TStringList;
+  JSONValue: TJSonValue;
+begin
+  StringList := TStringList.Create;
+  try
+    try
+      StringList.LoadFromFile(C_FILE_NAME);
+      JSONValue := TJSonObject.ParseJSONValue(StringList.Text);
+
+      JSONRequestRec.numero_casas := JSONValue.GetValue<Integer>
+        ('numero_casas');
+      JSONRequestRec.token := JSONValue.GetValue<string>('token');
+      JSONRequestRec.cifrado := JSONValue.GetValue<string>('cifrado');
+      JSONRequestRec.decifrado := JSONValue.GetValue<string>('decifrado');
+      JSONRequestRec.resumo_criptografico := JSONValue.GetValue<string>
+        ('resumo_criptografico');
+    finally
+      StringList.Free;
+    end;
+  except
+    on E: Exception do
+      Writeln('Ocorreu um problema ao carregar o arquivo JSON!');
+  end;
+end;
+
+function UpdateJSON: Boolean;
 var
   StringDecoded, SHA1: String;
+  JSONObject: TJSonObject;
 begin
-  Result:= False;
+  Result := False;
+  LoadJsonFile;
 
   if not JSONRequestRec.cifrado.IsEmpty then
   begin
-    StringDecoded:= Decode(JSONRequestRec.cifrado, JSONRequestRec.numero_casas);
-    SHA1:= THashSHA1.GetHashString(StringDecoded);
+    StringDecoded := Decode(JSONRequestRec.cifrado,
+      JSONRequestRec.numero_casas);
+    SHA1 := THashSHA1.GetHashString(StringDecoded);
 
     Writeln('String decodificada: ' + StringDecoded);
     Writeln('Resumo criptográfico - SHA1: ' + SHA1);
 
-    JSONRequestRec.decifrado           := StringDecoded;
-    JSONRequestRec.resumo_criptografico:= SHA1;
-    Result:= True;
-  end
-  else
-    Writeln('Não há mensagem para decifrar! Obtenha o JSON antes! Opção (1)');
+    JSONRequestRec.decifrado := StringDecoded;
+    JSONRequestRec.resumo_criptografico := SHA1;
 
-   Line;
-end;
-
-function GenerateJSONtoPost(var AJSONPost: string): Boolean;
-var
-  JSONObject: TJSONObject;
-begin
-  Result:= False;
-  JSONObject:= TJSONObject.Create;
-  try
-    if SetFieldValues then
-    begin
-      JSONObject.AddPair(TJSONPair.Create('numero_casas', JSONRequestRec.numero_casas.ToString));
+    JSONObject := TJSonObject.Create;
+    try
+      JSONObject.AddPair(TJSONPair.Create('numero_casas', TJSONNumber.Create(JSONRequestRec.numero_casas)));
       JSONObject.AddPair(TJSONPair.Create('token', JSONRequestRec.token));
       JSONObject.AddPair(TJSONPair.Create('cifrado', JSONRequestRec.cifrado));
       JSONObject.AddPair(TJSONPair.Create('decifrado', JSONRequestRec.decifrado));
       JSONObject.AddPair(TJSONPair.Create('resumo_criptografico', JSONRequestRec.resumo_criptografico));
-      AJSONPost:= JSONObject.ToJSON;
-      Result:= True;
+      SaveJSONtoFile(JSONObject.ToJSON);
+      JSONValue := TJSonObject.ParseJSONValue(JSONObject.ToJSON);
+      Result := True;
+    finally
+      JSONObject.Free;
     end;
-  finally
-    JSONObject.Free;
-  end;
+  end
+  else
+    Writeln('Não há mensagem para decifrar! Obtenha o JSON antes! Opção (1)');
+
+  Line;
 end;
 
 procedure PostJSON;
 var
-  JSONtoPost: string;
+  // JSONtoPost: string;
+  // JSONFile: TMemoryStream;
+  JSONFileStream: TFileStream;
+  JSONObject: TJSONObject;
 begin
-  if GenerateJSONtoPost(JSONtoPost) then
+  // if GenerateJSONtoPost(JSONtoPost) then]
+  if UpdateJSON then
   begin
-    RESTClient:= TRESTClient.Create(C_POST_URL);
-    RESTRequest:= TRESTRequest.Create(nil);
-    RESTResponse:= TRESTResponse.Create(nil);
+    // JSONFile:= TMemoryStream.Create;
+    JSONFileStream := TFileStream.Create(C_FILE_NAME, fmOpenRead);
+    RESTClient := TRESTClient.Create(C_POST_URL);
+    // RESTClient:= TRESTClient.Create('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution');
+    RESTRequest := TRESTRequest.Create(nil);
+    RESTResponse := TRESTResponse.Create(nil);
     try
       try
-        RESTRequest.Client:= RESTClient;
-        RESTRequest.Response:= RESTResponse;
-        RESTRequest.Resource:= C_POST_RESOURCE;
+        // JSONFile.LoadFromFile(C_FILE_NAME);
+        // JSONFile.Position:= 0;
+
+        //JSONFileStream.Position := 0;
+
+        RESTRequest.Client := RESTClient;
+        RESTRequest.Response := RESTResponse;
+        RESTRequest.Resource := C_POST_RESOURCE;
+        // RESTRequest.Params.AddHeader('Content-Type', 'multipart/form-data');
         RESTRequest.AddParameter('token', C_MY_TOKEN);
-        RESTRequest.AddBody(JSONtoPost, TRESTContentType.ctMULTIPART_FORM_DATA);
-        RESTRequest.Method:= rmPOST;
+        RESTRequest.AddParameter('file', TJSONObject(JSONValue), true);
+        //RESTRequest.AddFile('answer', C_FILE_NAME, TRESTContentType.ctMULTIPART_FORM_DATA);
+        // RESTRequest.Params.AddItem('file', C_FILE_NAME, pkGETorPOST, [], ctMULTIPART_FORM_DATA);
+        // RESTRequest.AddBody(JSONFile, TRESTContentType.ctMULTIPART_FORM_DATA);
+        RESTRequest.Method := rmPOST;
         RESTRequest.Execute;
+
+        RESTRequest.GetFullRequestURL(true);
 
         if RESTResponse.StatusCode = 200 then
         begin
@@ -167,16 +216,22 @@ begin
 
           Writeln('Status da submissão: ');
           Writeln(JSONValue.ToJSON);
-          Line;
+        end
+        else
+        begin
+          Writeln('Ocorreu um erro: ' + RESTResponse.Content);
         end;
+
+        Line;
       finally
-        RESTResponse.Free;
-        RESTRequest.Free;
-        RESTResponse.Free;
+        FreeAndNil(RESTResponse);
+        FreeAndNil(RESTRequest);
+        FreeAndNil(RESTClient);
+        JSONFileStream.Free;
       end;
     except
       on E: Exception do
-       Writeln('Ocorreu um erro ao submeter o JSON!');
+        Writeln('Ocorreu um erro ao submeter o JSON! Erro: ' + E.Message);
     end;
   end;
 end;
@@ -194,9 +249,8 @@ begin
 
       if Input = '1' then
         GetJSON
-      else
-      if Input = '2' then
-        PostJSON;
+      else if Input = '2' then
+        PostJSON; // UpdateJSON;//LoadJsonFile;
 
     until Input = '3';
 
@@ -206,4 +260,5 @@ begin
   finally
 
   end;
+
 end.
